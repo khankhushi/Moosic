63894ba3d093151b1e763e8a624739e7cc95f801
Khushi Khan <81975567+khankhushi@users.noreply.github.com>
2022-05-28 17:56:09 +0530

    Uploading streamlit app

---
+++ app.py
@@ -0,0 +1,128 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+
+import av
+import cv2
+import numpy as np
+import mediapipe as mp
+from keras.models import load_model
+import webbrowser
+
+
+
+st.set_page_config(layout="wide")
+st.image(".\Images\logo.png" , width=1370)
+st.title("Moodify")
+
+
+
+model  = load_model("model.h5")
+label = np.load("labels.npy")
+
+holistic = mp.solutions.holistic
+hands = mp.solutions.hands
+hol = holistic.Holistic()
+drawing = mp.solutions.drawing_utils
+
+
+
+
+if "run" not in st.session_state:
+	st.session_state["run"] = "true"
+try:
+    detected_emotion = np.load("detected_emotion.npy")[0]
+except:
+    detected_emotion = ""
+
+if not(detected_emotion):
+	st.session_state["run"] = "true"
+else:
+	st.session_state["run"] = "false"
+
+
+
+
+class EmotionDetector:
+    def recv(self, frame):
+        
+        frm = frame.to_ndarray(format="bgr24")
+
+        frm = cv2.flip(frm, 1) #Flipping the frame from left to right
+
+        res = hol.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB)) 
+
+        lst = []
+
+        #Storing Landmark data
+        if res.face_landmarks:
+            for i in res.face_landmarks.landmark:
+                    lst.append(i.x - res.face_landmarks.landmark[1].x)
+                    lst.append(i.y - res.face_landmarks.landmark[1].y)
+
+        if res.left_hand_landmarks:
+                for i in res.left_hand_landmarks.landmark:
+                    lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+        else:
+                for i in range(42):
+                    lst.append(0.0)
+
+        if res.right_hand_landmarks:
+                for i in res.right_hand_landmarks.landmark:
+                    lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+        else:
+                for i in range(42):
+                    lst.append(0.0)
+        
+        lst = np.array(lst).reshape(1,-1)
+
+        pred = label[np.argmax(model.predict(lst))]
+
+        print(pred)
+        cv2.putText(frm, pred, (50,50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0),2)
+        np.save("detected_emotion.npy", np.array([pred]))
+        
+
+        drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION)
+        drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+        drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+
+        return av.VideoFrame.from_ndarray(frm, format="bgr24")
+
+
+
+
+lang = st.text_input("Enter your preferred language")
+artist = st.text_input("Enter your preferred artist")
+
+if lang and artist and st.session_state["run"] != "false":
+    webrtc_streamer(key="key", desired_playing_state=True,
+				video_processor_factory=EmotionDetector)
+
+
+btn = st.button("Recommend music")
+
+if btn:
+	if not(detected_emotion):
+		st.warning("Please let me capture your emotion first!")
+		st.session_state["run"] = "true"
+	else:
+		webbrowser.open(f"https://www.youtube.com/results?search_query={lang}+{detected_emotion}+songs+{artist}")
+		np.save("detected_emotion.npy", np.array([""]))
+		st.session_state["run"] = "false"
+
+
+st.write('Made with ❤ by [Khushi](https://github.com/khankhushi/Moodify)')
+
+#Streamlit Customisation
+st.markdown(""" <style>
+div.stImage > 
+img{
+    align="center";
+}
+
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+
